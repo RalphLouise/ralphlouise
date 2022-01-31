a50ffcb951abae52d5ea3bef6d0f28a3aac32534
Ralph Louise <madaluraralphlouise@gmail.com>
2022-01-31 17:05:11 +0800

    PERFORMANCE TASK

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Dark",
+        artist: "Maroon",
+        img: "music-1-1",
+        src: "music-1-1",
+    },
+    {
+        name: "Light",
+        artist: "Maroon",
+        img: "music-1-2",
+        src: "music-1-2",
+    },
+    {
+        name: "Dark",
+        artist: "Yellow",
+        img: "music-2-1",
+        src: "music-2-1",
+    },
+    {
+        name: "Light",
+        artist: "Yellow",
+        img: "music-2-2",
+        src: "music-2-2",
+    },
+    {
+        name: "Dark",
+        artist: "Navy",
+        img: "music-3-1",
+        src: "music-3-1",
+    },
+    {
+        name: "Light",
+        artist: "Navy",
+        img: "music-3-2",
+        src: "music-3-2",
+    },
+    {
+        name: "Dark",
+        artist: "Green",
+        img: "music-4-1",
+        src: "music-4-1",
+    },
+    {
+        name: "Light",
+        artist: "Green",
+        img: "music-4-2",
+        src: "music-4-2",
+    },
+    {
+        name: "Dark",
+        artist: "Cyan",
+        img: "music-5-1",
+        src: "music-5-1",
+    },
+    {
+        name: "Light",
+        artist: "Cyan",
+        img: "music-5-2",
+        src: "music-5-2",
+    },
+    {
+        name: "Dark",
+        artist: "Violet",
+        img: "music-6-1",
+        src: "music-6-1",
+    },
+    {
+        name: "Light",
+        artist: "Violet",
+        img: "music-6-2",
+        src: "music-6-2",
+    },
+
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
